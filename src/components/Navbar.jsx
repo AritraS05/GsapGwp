6523beb5d315c65389b51b8279c18d5f10c1c4f5
Aritra <aritrasarkar00007@gmail.com>
2024-12-29 01:42:24 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Button from './Button';
+import { TiLocationArrow } from 'react-icons/ti';
 
 const Navbar = () => {
+
+    const navConatainerRef = useRef(null);
   return (
-    <div>Navbar</div>
+    <div ref={navConatainerRef} className='fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6'>
+        <header className='absolute top-1/2 w-full -transalate-y-1/2'>
+            <nav className='flex size-full items-center justify-between p-4'>
+                <div className='flex items-center gap-7'>
+                    <img 
+                        src='/img/logo.png'
+                        alt='logo'
+                        className='w-10'
+                    />
+                    <Button 
+                        id="product-button"
+                        title="Product"
+                        rightIcon={<TiLocationArrow />}
+                        containerClass="bg-blue-50 md:flex hidden"
+                    />
+                </div>
+            </nav>
+        </header>
+    </div>
   )
 }
 
